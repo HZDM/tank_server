a20f09f1eb31bb6a6597d65ea644dfb3396a5ec0
yangwunan <yangwunan0608@gmail.com>
2017-05-22 23:07:14 +0800

    mod socket server to support 2 ports add main func template

--- tank_server.py
+++ tank_server.py
@@ -3,60 +3,82 @@ import argparse
 import socket
 import select
 import queue
+import copy
+
 
 class sock_server(object):
-    def __init__(self, port):
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.setblocking(0)
-        self.server.bind(('0.0.0.0', port))
-        self.server.listen(5)
-        self.inputs = [self.server]
-        self.outputs = []
-        self.message_queues = {}
+    def __init__(self, port_list):
+        self.servers = []
 
-    def start(self):
+        for port in port_list:
+            server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server.setblocking(0)
+            server.bind(('0.0.0.0', port))
+            server.listen(1)
+            self.servers.append(server)
+
+        self.inputs = [self.servers[0], self.servers[1]]
+        self.outputs = []
+        self.recv_message_queues = {}
+        self.send_message_queues = {}
+
+    def start(self, func):
         inputs = self.inputs
         outputs = self.outputs
-        message_queues = self.message_queues
+        recv_message_queues = self.recv_message_queues
+        send_message_queues = self.send_message_queues
 
-        while inputs:
-            readable, writable, exceptional = select.select(
-                inputs, outputs, inputs)
-            for s in readable:
-                if s is self.server:
-                    connection, client_address = s.accept()
-                    connection.setblocking(0)
-                    inputs.append(connection)
-                    message_queues[connection] = queue.Queue()
-                else:
-                    data = s.recv(1024)
-                    if data:
-                        message_queues[s].put(data)
-                        if s not in outputs:
-                            outputs.append(s)
+        try:
+            while inputs:
+                readable, writable, exceptional = select.select(inputs, outputs, inputs)
+
+                for s in readable:
+                    if s in self.servers:
+                        connection, client_address = s.accept()
+                        connection.setblocking(0)
+                        inputs.append(connection)
+                        recv_message_queues[connection] = queue.Queue()
+                        send_message_queues[connection] = queue.Queue()
                     else:
-                        if s in outputs:
-                            outputs.remove(s)
-                        inputs.remove(s)
-                        s.close()
-                        del message_queues[s]
+                        data = s.recv(1024)
+                        if data:
+                            recv_message_queues[s].put(data)
+                            if s not in outputs:
+                                outputs.append(s)
+                        else:
+                            if s in outputs:
+                                outputs.remove(s)
+                            inputs.remove(s)
+                            s.close()
+                            del recv_message_queues[s]
+                            del send_message_queues[s]
 
-            for s in writable:
-                try:
-                    next_msg = message_queues[s].get_nowait()
-                except queue.Empty:
-                    outputs.remove(s)
-                else:
-                    s.send(next_msg)
+                for s in writable:
+                    try:
+                        next_msg = send_message_queues[s].get_nowait()
+                    except queue.Empty:
+                        outputs.remove(s)
+                    else:
+                        s.send(next_msg)
 
-            for s in exceptional:
-                inputs.remove(s)
-                if s in outputs:
-                    outputs.remove(s)
+                for s in exceptional:
+                    inputs.remove(s)
+                    if s in outputs:
+                        outputs.remove(s)
+                    s.close()
+                    del recv_message_queues[s]
+                    del send_message_queues[s]
+
+                func()
+
+        except Exception as e:
+            print(e)
+            pass
+        finally:
+            all_opened_socket = set(self.inputs + self.outputs + self.servers)
+            for s in all_opened_socket:
+                print(s)
                 s.close()
-                del message_queues[s]
-
-
 
 
 def args_process():
@@ -73,11 +95,37 @@ def args_process():
     pass
 
 
+state = 0
+
+
+def move_state(new_state):
+    global state
+    state = new_state
+
+
+state_func = {
+    'server_start': None,
+    'wait_for_players': None,
+    'game_start': None,
+    'leg_start': None,
+    'leg_end': None,
+    'game_end': None
+}
+
+
+def main_process():
+    if state_func[state]:
+        state_func[state]()
+
+
 def main():
-    args = args_process() # get arg by dicts
+    args = args_process()  # get arg by dicts
     print(args)
 
-    sock_server(25555)
+    ss = sock_server([args['port1'], args['port2']])
+
+    move_state('server_start')
+    ss.start(main_process)
 
     pass
 
